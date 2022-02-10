763c41c1a32add37ab67f4cba5f3f96829c9d92e
PJ Jones <pj.jones4000.com>
2022-02-10 11:35:00 -0600

    everything works but sometimes the sumDice button messes up

--- scripts.js
+++ scripts.js
@@ -6,7 +6,13 @@ let sum = 0;
 let addDie = () => {
   let squareDiv = document.createElement("div");
   squareDiv.className = "die";
-  squareDiv.id = j;
+  for (let l = 0; l < dieArr.length +1; l++) {
+    if (!dieArr) {
+      squareDiv.id = l;
+    }
+    squareDiv.id = l;
+  }
+  // squareDiv.id = j;
   document.getElementById("dieDiv").appendChild(squareDiv);
   let squarep = document.createElement("p");
   squareDiv.appendChild(squarep);
@@ -18,7 +24,7 @@ let addDie = () => {
       // document.createTextNode(this.value);
       squarep.innerText = this.value;
       Die.value = this.value;
-      
+
       // console.log(this.value);
 
       // squareDiv.id = i;
@@ -37,32 +43,32 @@ let addDie = () => {
 document.getElementById("genBtn").addEventListener("click", addDie);
 document.getElementById("rollBtn").addEventListener("click", function () {
   for (let k = 0; k < dieArr.length; k++) {
-    dieArr[k].roll();    
+    dieArr[k].roll();
   }
   console.log(dieArr);
 });
 
 document.getElementById("sumDice").addEventListener("click", function () {
   for (let k = 0; k < dieArr.length; k++) {
+    console.log(dieArr[k].value)
     sum += dieArr[k].value;
   }
   alert(`the sum is ${sum}.`);
   sum = 0;
 });
 
-document.body.addEventListener("click", function(event) {
+document.body.addEventListener("click", function (event) {
   if (event.target.className === "die") {
-    dieArr[event.target.id -1].roll();
-    
+    console.log(event.target.id);
+    dieArr[event.target.id].roll();
   }
 });
 
-document.body.addEventListener("dblclick", function(event) {
+document.body.addEventListener("dblclick", function (event) {
   if (event.target.className === "die") {
     let remove = document.getElementById(event.target.id);
-    remove.remove()
+    remove.remove();
     dieArr.splice(dieArr.indexOf(event.target), 1);
     console.log(dieArr);
   }
 });
-
